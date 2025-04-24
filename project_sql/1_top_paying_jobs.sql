7f73dc2af0483c8c238ee248f42b1e54acf694f3
chibee-c <chibeeofficial@gmail.com>
2025-04-24 09:55:47 -0600

    created top_10_jobs file

---
+++ project_sql/1_top_paying_jobs.sql
@@ -0,0 +1,9 @@
+/*
+What are the top-paying jobs for the Data Analyst role?
+- Identify the top 10 highest-paying Data Analyst roles that are available remotely.
+- Focus on job postings with specified salaries (remove nulls)
+- Why? Highlight the top-paying opportunities for Data Analysts, offering insights into emplo
+*/
+
+
+SELECT 
